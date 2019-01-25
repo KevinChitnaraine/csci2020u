f8859938f9ffadd5b739010b4394b6b4b9002895
Kevin Chitnaraine <kevin.chitnaraine@uoit.net>
2019-01-25 08:47:34 -0500

    beta

--- lab01/Battle.java
+++ lab01/Battle.java
@@ -1,8 +1,8 @@
 public class Battle {
 	public static void main(String[] args) {
-		float kroggAttack = 38.5f;
-		float kroggDefense = 20.0f;
-		float kroggHP = 200f;
+		float alexAttack = 38.5f;
+		float alexDefense = 20.0f;
+		float alexHP = 200f;
 
 		float bossAttack = 25f;
 		float bossDefense = 12.5f;
@@ -10,25 +10,25 @@ public class Battle {
 
 		int round = 1;
 		
-		while ((kroggHP > 0) && (bossHP > 0)) {
+		while ((alexHP > 0) && (bossHP > 0)) {
 			System.out.println("\nRound " + round);
 			
-			float kroggDamage = kroggAttack - bossDefense;
-			bossHP -= kroggDamage;
-			System.out.printf("Krogg does %f points of damage to Boss\n", kroggDamage);
+			float alexDamage = alexAttack - bossDefense;
+			bossHP -= alexDamage;
+			System.out.printf("alex does %f points of damage to Boss\n", alexDamage);
 
-			float bossDamage = bossAttack - kroggDefense;
-			kroggHP -= bossDamage;
-			System.out.printf("Boss does %f points of damage to Krogg\n", bossDamage);
+			float bossDamage = bossAttack - alexDefense;
+			alexHP -= bossDamage;
+			System.out.printf("Boss does %f points of damage to alex\n", bossDamage);
 			
 			System.out.println("Boss:  " + bossHP);
-			System.out.println("Krogg: " + kroggHP);
+			System.out.println("alex: " + alexHP);
 
 			round++;
 		}
 		
-		if (kroggHP > 0) {
-			System.out.println("\nKrogg has won.  You are victorious!");
+		if (alexHP > 0) {
+			System.out.println("\nalex has won.  You are victorious!");
 		} else {
 			System.out.println("\nBoss has won.  You have been defeated.");
 		}
